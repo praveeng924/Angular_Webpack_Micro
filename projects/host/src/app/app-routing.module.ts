f0cd94bf423ce5b141737c1d947dcdc3bff959d0
praveeng924 <praveengopakumar1@gmail.com>
2021-07-05 22:37:01 +0530

    sample-project-completed

--- projects/host/src/app/app-routing.module.ts
+++ projects/host/src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{path:'',component:HomeComponent,pathMatch:'full'}];
+const routes: Routes = [{path:'',component:HomeComponent,pathMatch:'full'},{
+  path: 'mfe1',
+  loadChildren: () =>
+    import('mfe1/MfefeatureModule').then((m) => {
+      return m.MfefeatureModule;
+    }).catch((err)=>{console.log(err.message);}),
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
