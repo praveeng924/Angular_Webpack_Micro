364579f4f2d40f77f1199098d61d17ecf3170e08
praveeng924 <praveengopakumar1@gmail.com>
2021-07-05 21:17:19 +0530

    application phase 1 completed

--- projects/mfe1/src/app/mfefeature/mfefeature-routing.module.ts
+++ projects/mfe1/src/app/mfefeature/mfefeature-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MfefeatureComponent } from './mfefeature.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:'',component:MfefeatureComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
